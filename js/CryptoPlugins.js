83a3bf4517a649504c7eb3e006a798e286d9901c
woodser <woodser@protonmail.com>
2017-09-01 15:37:31 -0400

    Prototyping

--- js/CryptoPlugins.js
+++ js/CryptoPlugins.js
@@ -1,45 +1,121 @@
+var plugins;
+function getCryptoPlugins() {
+	if (!plugins) {
+		plugins = [];
+		plugins.push(new BitcoinPlugin());
+//		plugins.push(new EthereumPlugin());
+		plugins.push(new MoneroPlugin());
+//		plugins.push(new LitecoinPlugin());
+//		plugins.push(new BitcoinCashPlugin());
+//		plugins.push(new EthereumClassicPlugin());
+	}
+	return plugins;
+}
+
+function getCryptoPlugin(ticker) {
+	for (let plugin of getCryptoPlugins()) {
+		if (plugin.getTickerSymbol() === ticker) return plugin;
+	}
+	return null;
+}
+
 /**
  * Base plugin that specific cryptocurrencies must implement.
  */
-function CryptoPlugin {
+function CryptoPlugin { }
+
+/**
+ * Returns the name.
+ */
+CryptoPlugin.prototype.getName = function() { throw new Error("Subclass must implement"); }
+
+/**
+ * Returns the ticker symbol.
+ */
+CryptoPlugin.prototype.getTickerSymbol = function() { throw new Error("Subclass must implement"); }
+
+/**
+ * Returns the logo.
+ */
+CryptoPlugin.prototype.getLogo = function() { throw new Error("Subclass must implement"); }
 	
-	/**
-	 * Returns the cryptocurrency's name.
-	 */
-	this.getName = function() { throw new Error("Subclass must implement"); }
+/**
+ * Returns the supported encryption schemes.
+ * 
+ * Supports CryptoJS by default.
+ */
+CryptoPlugin.prototype.getEncryptionSchemes = function() { return [EncryptionScheme.CRYPTOJS;] }
+
+/**
+ * Returns a new random private key.
+ */
+CryptoPlugin.prototype.newPrivateKey = function() { return new PrivateKey(this); }
 	
-	/**
-	 * Returns the cryptocurrency's ticker symbol.
-	 */
-	this.getTickerSymbol = function() { throw new Error("Subclass must implement"); }
+/**
+ * Returns a new unencrypted hex private key.
+ */
+CryptoPlugin.prototype.newUnencryptedPrivateKeyHex = function() { throw new Error("Subclass must implement"); }
+
+/**
+ * Converts the given private key from hex to wif.
+ */
+CryptoPlugin.prototype.privateKeyHexToWif = function(hex) { throw new Error("Subclass must implement"); }
+
+/**
+ * Converts the given private key from wif to hex.
+ */
+CryptoPlugin.prototype.privateKeyWifToHex = function(wif) { throw new Error("Subclass must implement"); }
 	
-	/**
-	 * Returns the cryptocurrency's logo.
-	 */
-	this.getLogo = function() { throw new Error("Subclass must implement"); }
+/**
+ * Determines if the given string is a hex or wif private key, encrypted or unencrypted.
+ */
+CryptoPlugin.prototype.isPrivateKey = function(str) { return this.isPrivateKeyHex() || this.isPrivateKeyWif(); }
 	
-	/**
-	 * Returns the cryptocurrency's supported encryption schemes.
-	 */
-	this.getEncryptionSchemes = function() {
-		return EncryptionScheme.CRYPTOJS;	// every currency supports cryptojs
-	}
+/**
+ * Determines if the given string is a hex private key, encrypted or unencrypted.
+ */
+CryptoPlugin.prototype.isPrivateKeyHex = function(str) { throw new Error("Subclass must implement"); }
 	
-	/**
-	 * Returns the encryption scheme of the given private key string.  Throws an exception if the string is not recognized.
-	 */
-	this.getEncryptionScheme = function(str) { throw new Error("Subclass must implement"); }
+/**
+ * Determines if the given string is a wif private key, encrypted or unencrypted.
+ */
+CryptoPlugin.prototype.isPrivateKeyWif = function(str) { throw new Error("Subclass must implement"); }
 	
-	/**
-	 * Returns the address of the given private key.
-	 */
-	this.getAddress = function(key) { throw new Error("Subclass must implement"); }
-}
+/**
+ * Determines if the given string is an encrypted hex or wif private key.
+ */
+CryptoPlugin.prototype.isEncryptedPrivateKey = function(str) { try { return isInitialized(this.getEncryptionScheme(str)); } catch {} }
+	
+/**
+ * Returns the encryption scheme of the given private key string.  Throws an exception if the string is not recognized.
+ */
+CryptoPlugin.prototype.getEncryptionScheme = function(str) { throw new Error("Subclass must implement"); }
+	
+/**
+ * Returns the address of the given private key.
+ */
+CryptoPlugin.prototype.getAddress = function(privateKey) { throw new Error("Subclass must implement"); }
+
 
 function BitcoinPlugin {
-	
+	this.getName = function() { return "Bitcoin"; }
+	this.getTickerSymbol = function() { return "BTC" };
+	this.getLogo = function() { return $("<img src='img/bitcoin.png'>"); }
+	this.getEncryptionSchemes = function() { return [EncryptionScheme.BIP38, EncryptionScheme.CRYPTOJS]; }
+	this.newUnencryptedPrivateKeyHex = function() {
+		var key = new Bitcoin.ECKey(false);	// bitaddress.js:5367	// TODO: handle randomization
+		key.setCompressed(true);
+		return key.getBitcoinHexFormat();
+	}
+	this.privateKeyHexToWif = function(hex) {
+		var key = new Bitcoin.ECKey(hex);	// bitaddress.js:5367	// TODO: handle randomization
+		key.setCompressed(true);
+		return key.getBitcoinHexFormat();
+	}
 }
+inheritsFrom(BitcoinPlugin, CurrencyPlugin);
 
 function MoneroPlugin {
 	
 }
+inheritsFrom(MoneroPlugin, CurrencyPlugin);
---
+++ js/PrivateKey.js
@@ -0,0 +1,93 @@
+/**
+ * Maintains a single private key.
+ * 
+ * @param plugin is a plugin for a specific cryptocurrency
+ * @param state defines the initial state (optional)
+ */
+function PrivateKey(plugin, state) {
+	
+	// initialize key
+	if (!isInitialized(plugin) || typeof plugin !== 'object' || plugin.constructor.name !== 'CryptoPlugin') throw new Error("Must provide crypto plugin");
+	this.plugin = plugin;
+	this.state = {};
+	if (state) this.setState(state);
+	else this.random();
+	
+	this.copy = function() {
+		return new CryptoKey(this.plugin, this.state);
+	}
+	
+	this.equals = function(cryptoKey) {
+		return mapsEqual(this.getState(), cryptoKey.getState());
+	}
+	
+	this.getPlugin = function() {
+		return this.plugin;
+	}
+	
+	this.getState = function() {
+		return this.state;
+	}
+	
+	this.random = function() {
+		this.setPrivateKey(this.plugin.newPrivateKeyHex());
+	}
+	
+	this.setPrivateKey(str) {
+		this.setState({privateKey: str});
+	}
+	
+	this.toAddress = function() {
+		return this.state.address;
+	}
+	
+	this.toHex = function() {
+		return this.state.hex;
+	}
+
+	this.toWif = function() {
+		return this.state.wif;
+	}
+	
+	this.encrypt = function(scheme, password) {
+		throw new Error("Not implemented");
+	}
+	
+	this.decrypt = function(password) {
+		throw new Error("Not implemented");
+	}
+	
+	this.isEncrypted = function() {
+		return isInitialized(this.state.encryption);
+	}
+	
+	this.getEncryptionScheme = function() {
+		return this.state.encryption;
+	}
+	
+	this.setState = function(state) {
+		// copy state
+		this.state = Object.assign({}, state);
+		
+		// set private key
+		if (state.privateKey) {
+			if (plugin.isPrivateKeyHex(state.privateKey)) {
+				this.state.hex = privateKey;
+				this.state.wif = plugin.privateKeyHexToWif(state.privateKey);
+			} else if (plugin.isPrivateKeyWif(state.privateKey)) {
+				this.state.hex = plugin.privateKeyWifToHex(state.privateKey);
+				this.state.wif = state.privateKey;
+			} else throw new Error("Unrecognized private key: " + state.privateKey);
+		}
+		
+		// set encryption
+		this.state.encryption = plugin.getEncryptionScheme(state.privateKey);
+		
+		// set address
+		if (!this.isEncrypted) {
+			let address = plugin.getAddress(this);
+			if (state.address && state.address !== address) throw new Error("state.address does not match derived address");
+			this.state.address = address;
+		}
+	}
+}
