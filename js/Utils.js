4ffe29a8bf187a6ac6afd4d144e33e49f23b3433
woodser <woodser@protonmail.com>
2017-09-04 14:10:11 -0400

    WIP cryptojs decryption

--- js/Utils.js
+++ js/Utils.js
@@ -636,10 +636,16 @@ function decrypt(key, password, callback) {
 	if (!password) throw new Error("Password must be initialized");
 	switch (key.getEncryptionScheme()) {
 		case EncryptionScheme.CRYPTOJS:
+			console.log(key.toWif());
+			console.log(password);
+			console.log(CryptoJS.AES.decrypt(key.toWif(), password));
 			let hex = CryptoJS.AES.decrypt(key.toWif(), password).toString(CryptoJS.enc.Utf8);
-			key.setState(key.getPlugin().parse(hex).getState());
-			callback(key);
-			
+			console.log(hex);
+			if (!hex) callback(undefined, new Error("Invalid password"));
+			else {
+				key.setState(key.getPlugin().parse(hex).getState());
+				callback(key);
+			}
 			
 //			throw new Error("Not implemented fool");
 //			CryptoJS.enc.Hex.parse(key.toHex()).toString(CryptoJS.enc.Base64).toString(CryptoJS.enc.Utf8);
