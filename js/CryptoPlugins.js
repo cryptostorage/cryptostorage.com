684cdfba4278b40b6e81c20614cb9f431488cac5
woodser <woodser@protonmail.com>
2017-09-04 16:32:23 -0400

    WIP MoneroPlugin

--- js/CryptoPlugins.js
+++ js/CryptoPlugins.js
@@ -2,9 +2,9 @@ var plugins;
 function getCryptoPlugins() {
 	if (!plugins) {
 		plugins = [];
-		plugins.push(new BitcoinPlugin());
+//		plugins.push(new BitcoinPlugin());
 //		plugins.push(new EthereumPlugin());
-//		plugins.push(new MoneroPlugin());
+		plugins.push(new MoneroPlugin());
 //		plugins.push(new LitecoinPlugin());
 //		plugins.push(new BitcoinCashPlugin());
 //		plugins.push(new EthereumClassicPlugin());
@@ -154,5 +154,69 @@ function MoneroPlugin() {
 	this.getName = function() { return "Monero"; }
 	this.getTickerSymbol = function() { return "XMR" };
 	this.getLogo = function() { return $("<img src='img/monero.png'>"); }
+	this.newKey = function(str) {
+		
+		// create new key
+		if (!str) {
+			let state = {};
+			state.hex = cnUtil.sc_reduce32(cnUtil.rand_32());
+			state.wif = mn_encode(state.hex, 'english');
+			state.address = cnUtil.create_address(state.hex).public_addr;
+			state.encryption = null;
+			return new CryptoKey(this, state);
+		}
+		
+		// parse key
+		assertTrue(isString(str), "Argument to parse must be a string");
+		let state = {};
+		
+		// unencrypted private key
+		if (ninja.privateKey.isPrivateKey(str)) {
+			let key = new Bitcoin.ECKey(str);
+			key.setCompressed(true);
+			state.hex = key.getBitcoinHexFormat();
+			state.wif = key.getBitcoinWalletImportFormat();
+			state.address = key.getBitcoinAddress();
+			state.encryption = null;
+		}
+		
+		// wif bip38
+		else if (ninja.privateKey.isBIP38Format(str)) {
+			state.hex = Crypto.util.bytesToHex(Bitcoin.Base58.decode(str));
+			state.wif = str;
+			state.encryption = EncryptionScheme.BIP38;
+		}
+		
+		// wif cryptojs
+		else if (str[0] === 'U') {
+			state.hex = CryptoJS.enc.Base64.parse(str).toString(CryptoJS.enc.Hex);
+			state.wif = str;
+			state.encryption = EncryptionScheme.CRYPTOJS;
+		}
+		
+		// encrypted hex
+		else if (isHex(str)) {
+			
+			// bip38
+			if (str.length > 80 && str.length < 90) {
+				state.hex = str;
+				state.wif = Bitcoin.Base58.encode(Crypto.util.hexToBytes(str));
+				state.encryption = EncryptionScheme.BIP38;
+			}
+			
+			// cryptojs
+			else if (str.length > 100) {
+				state.hex = str;
+				state.wif = CryptoJS.enc.Hex.parse(str).toString(CryptoJS.enc.Base64).toString(CryptoJS.enc.Utf8);
+				state.encryption = EncryptionScheme.CRYPTOJS;
+			}
+		}
+		
+		// otherwise key is not recognized
+		else throw new Error("Unrecognized private key: " + str);
+		
+		// return key
+		return new CryptoKey(this, state);
+	}
 }
 inheritsFrom(MoneroPlugin, CryptoPlugin);
