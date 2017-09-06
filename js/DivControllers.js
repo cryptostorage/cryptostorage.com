520779cdca129ec0b085618b8040fbc3f73c2e15
woodser <woodser@protonmail.com>
2017-09-06 13:56:51 -0400

    Home screen wording

--- js/DivControllers.js
+++ js/DivControllers.js
@@ -160,9 +160,9 @@ function HomeController(div, onSelectCreate, onSelectImport) {
 		
 		div.append(getCheckmarkDiv("Generate secure storage for multiple cryptocurrencies."));
 		div.append(getCheckmarkDiv("Protect your private keys with password encryption."));
-		div.append(getCheckmarkDiv("Optionally split your private keys into separate pieces for additional security."));
+		div.append(getCheckmarkDiv("Optionally split your private keys into separate pieces which must be combined."));
+		div.append(getCheckmarkDiv("100% open source, client-side, and free to use.  No registration or trusted third parties."));
 		div.append(getCheckmarkDiv("Export to printable QR codes, csv, json, and txt for long term storage and easy recovery."));
-		div.append(getCheckmarkDiv("100% open source, client-side, and free to use.  No account or trusted third parties."));
 		div.append("<br>");
 		
 		div.append("Select an option to get started.")
