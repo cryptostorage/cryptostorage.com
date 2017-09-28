6be06eeb1dc82000426f664f2b327e1654a04f90
woodser <woodser@protonmail.com>
2017-09-28 10:44:12 -0400

    todo

--- js/AppController.js
+++ js/AppController.js
@@ -1,4 +1,5 @@
 // TODO
+// fetch mix dependencies as soon as known
 // solve issue where next page may take time to load, but previous page is done
 // prevent rendered page from showing after home, FAQ or donate clicked
 // home button maintains state, doesn't start over
