8f53c6332ba01ac82438cb63ef9c1bdbd3855eaa
woodser <woodser@protonmail.com>
2017-10-20 12:19:01 -0400

    smaller images don't lazy load slider

--- js/DivControllers.js
+++ js/DivControllers.js
@@ -133,10 +133,10 @@ function SliderController(div, onSelectGenerate, onSelectRecover) {
 		let sliderContainerDiv = $("<div class='slider_container'>").appendTo(div);
 		let sliderDiv = $("<div class='single-item'>").appendTo(sliderContainerDiv);
 		getSlide($("<img src='img/mix.png'>"), "Generate keys for multiple cryptocurrencies.").appendTo(sliderDiv);
-		getSlide($("<img data-lazy='img/security.png'>"), "Keys are generated in your browser so funds are never entrusted to a third party.").appendTo(sliderDiv);
-		getSlide($("<img data-lazy='img/search_file.png'>"), "100% open source and free to use.  No account necessary.").appendTo(sliderDiv);
-		getSlide($("<img data-lazy='img/password_protected.png'>"), "Private keys can be password protected and split into pieces.").appendTo(sliderDiv);
-		getSlide($("<img data-lazy='img/printer.png'>"), "Export to digital and printable formats for long term storage and easy recovery.").appendTo(sliderDiv);
+		getSlide($("<img src='img/security.png'>"), "Keys are generated in your browser so funds are never entrusted to a third party.").appendTo(sliderDiv);
+		getSlide($("<img src='img/search_file.png'>"), "100% open source and free to use.  No account necessary.").appendTo(sliderDiv);
+		getSlide($("<img src='img/password_protected.png'>"), "Private keys can be password protected and split into pieces.").appendTo(sliderDiv);
+		getSlide($("<img src='img/printer.png'>"), "Export to digital and printable formats for long term storage and easy recovery.").appendTo(sliderDiv);
 		sliderDiv.slick({autoplay:!DEBUG, arrows:false, dots:true, autoplaySpeed:4000});
 		
 		function getSlide(img, text) {
