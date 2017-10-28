f4789885c76b9ebd2fc952b8a2c073888d889812
woodser <woodser@protonmail.com>
2017-10-27 23:32:32 -0400

    print working well on chrome

--- js/DivControllers.js
+++ js/DivControllers.js
@@ -492,7 +492,7 @@ function FormController(div) {
 	/**
 	 * Generates pieces based on the current configuration and updates the GUI.
 	 */
-	function onGeneratePieces(onDone) {
+	function onGeneratePieces(window, onDone) {
 		generateKeys(function(done, total, label) {
 			progressBar.set(done / total);
 			if (label) progressBar.setText(label);
