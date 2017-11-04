419e6164e00903b261d3bf844c8c0577e75e11c6
woodser <woodser@protonmail.com>
2017-11-04 10:33:05 -0400

    organize all todos

--- js/AppController.js
+++ js/AppController.js
@@ -1,73 +1,58 @@
 // TODO
-// all the error messages
-// memory bug in bip38 encryption
-// num keys input flashes on first load
-// cannot delete first currency
+// pagination
+// homepage
 // faq
-// donate
-// label keys with pieces e.g. #1.3, #2.3, #3.3
-// peer review key formats
-// piece export: print, download, select pieces
-// faster load time
-
-// generate export tab, then in source change to import, export tab no longer works
-// public address "decrypt private key to view"
-// ddslick pulldown loading before homepage loads, dependency management
-// load all of home page when all images loaded
+// tab stops working if source closed
+// export controller progress design, initial and re-render
+// input errors
+// piece numbering in data
+// bip38
+// wait for images before loading homepage
+// don't wait for recover / ddslick before homepage
+// harden inputs: >0, must have first currency, etc
+// public address (decrypt to view) instead of (omitted)
 // plugin.newKey() returns null for unrecognized strs instead of throws error
 // ios print spans more than one page
-// was able to print missing pair divs with large number of keys (200)
-// low priority: try all imported file / key combinations to recover
-// homepage scale too big on small screens
-// prevent scroll revealing some of next picture
-
-// use @media to dynamically space piece pages
 // todos throughout code
 // run minimum tests when site accessed
 // test alert if key creation fails for any reason
 // verify no name collisions
 // file import zip with invalid json shouldn't prevent others from importing
-// wording on front page / be consistent
 // condense files to single files as much as possible
 // warning if live and/or online
 // test on IE
-// smaller html exports
 // note of how b64-images.js is created
 // how does jaxx generate zcash and bitcoin variations
-// warning to discard storage
+// warning to discard storage if not saved or printed
 // double click keys should select all
-// hide page breaks in previews with @media print
-// copy line break in pdf inserts space
-// import html / other formats?
 // restrict file types for file picker
+
+// design:
+// homepage scale too big on small screens
+
+// low priority:
+// try all imported file / key combinations to recover
+// prevent scroll revealing some of next picture
+// copy line break in pdf inserts space
 // code scan
+// console warning "unexpected end of file" in firefox caused by ddslick
+// timeouts in tests so browser doesn't lock
+// csv import to support "bring your own keys"
+// optimize tests by getting one set of keys of repeat_long size
 
 // consult designers
 // opinion on donate page + ui designer tip address - link to open user's wallet
 // color of 'c' within cs logo - little bit lighter
 // error messages on key generation page - let them click button once, outline with red, deactivate until they fix.  Constrain split input and keep in sync
-// disable generate button until input valid?
+// disable generate button until input valid? - let them click once then disable until error corrected
 // color of generate buttons / pulsing? - form button same as home page, maybe shift to darker
 // slider points, logos, and supporting content - fill icons with white, multiple locks
 // BIP38 vs CryptoJS encryption option for BTC/BCH - checkbox on same line as "Show passphrase", "Use BIP38 for Bitcoin and Bitcoin Cash"
-// export flow after key generation + options
-// import flow to read zip + import from private key
-// how to handle progress bar + status during key generation - hijack button to display both progress and status
 // live site warning to stop and download from github - orange bar on form page encouraging users to download
-// use tabs for pieces?
 // mobile static frame?
 // password confirmation - re-enter or show
 // title bar should not scroll?
 // warnings to test before using - add FAQ
-// code review
-
-// low priority
-// console warning "unexpected end of file" in firefox caused by ddslick
-// html export is horizontally centered with @media print for actual print
-// timeouts in tests so browser doesn't lock
-// csv import to support "bring your own keys"
-// optimize tests by getting one set of keys of repeat_long size
-// depressed blue format on hover for links - why does it stay hovered?
 
 const RUN_TESTS = false;
 const DEBUG = true;
