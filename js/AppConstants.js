f625d66eb9633574593085268576c66ef0995c7d
woodser <woodser@protonmail.com>
2017-11-06 11:15:14 -0500

    new files

---
+++ js/AppConstants.js
@@ -0,0 +1,5 @@
+const RUN_TESTS = false;
+const DEBUG = true;
+const DELETE_WINDOW_CRYPTO = false;
+const VERIFY_ENCRYPTION = false;
+const APP_DEPENDENCIES = ["lib/qrcode.js", "lib/jszip.js", "lib/crypto-js.js", "lib/bitaddress.js", "lib/progressbar.js", "lib/jquery.ddslick.js"];
