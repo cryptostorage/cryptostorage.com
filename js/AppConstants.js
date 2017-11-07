324bcd471f06877103d4730f52c0570af2cadb11
woodser <woodser@protonmail.com>
2017-11-06 20:51:10 -0500

    fix issue on import key decryption

--- js/AppConstants.js
+++ js/AppConstants.js
@@ -2,4 +2,4 @@ const RUN_TESTS = false;
 const DEBUG = true;
 const DELETE_WINDOW_CRYPTO = false;
 const VERIFY_ENCRYPTION = false;
-const APP_DEPENDENCIES = ["lib/qrcode.js", "lib/jszip.js", "lib/crypto-js.js", "lib/bitaddress.js", "lib/progressbar.js", "lib/jquery.ddslick.js"];
+const APP_DEPENDENCIES = ["lib/qrcode.js", "lib/jszip.js", "lib/crypto-js.js", "lib/bitaddress.js", "lib/progressbar.js", "lib/jquery.ddslick.js", "lib/b64-images.js"];
