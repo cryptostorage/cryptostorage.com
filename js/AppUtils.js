f77ef019dd2350dbf949730600f457224a19c9e6
woodser <woodser@protonmail.com>
2018-01-03 16:56:37 -0500

    play with ordering

--- js/AppUtils.js
+++ js/AppUtils.js
@@ -194,14 +194,14 @@ var AppUtils = {
 	getCryptoPlugins: function() {
 		if (!AppUtils.plugins) {
 			AppUtils.plugins = [];
-			AppUtils.plugins.push(new BitcoinPlugin());
+			AppUtils.plugins.push(new BitcoinCashPlugin());
 			AppUtils.plugins.push(new EthereumPlugin());
 			AppUtils.plugins.push(new MoneroPlugin());
-			AppUtils.plugins.push(new BitcoinCashPlugin());
+			AppUtils.plugins.push(new BitcoinPlugin());
 			AppUtils.plugins.push(new LitecoinPlugin());
-			AppUtils.plugins.push(new OmiseGoPlugin());
 			AppUtils.plugins.push(new DashPlugin());
 			AppUtils.plugins.push(new ZcashPlugin());
+			AppUtils.plugins.push(new OmiseGoPlugin());
 			AppUtils.plugins.push(new BasicAttentionTokenPlugin());
 			AppUtils.plugins.push(new EthereumClassicPlugin());
 		}
