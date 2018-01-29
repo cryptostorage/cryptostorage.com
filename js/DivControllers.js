498fd9f996a2fc2a2736efe0a2f8f553a2518bfe
woodser <woodser@protonmail.com>
2018-01-29 15:12:42 -0500

    wording

--- js/DivControllers.js
+++ js/DivControllers.js
@@ -327,7 +327,7 @@ function IntroController(div, onSelectGenerate, onSelectImport) {
 			getSlide($("<img src='img/printer.png'>"), "Print paper wallets for long-term storage.").appendTo(sliderDiv);
 			getSlide($("<img src='img/security.png'>"), "Runs only in your browser so funds are never entrusted to a third party.").appendTo(sliderDiv);
 			getSlide($("<img src='img/microscope.png'>"), "100% open-source and free to use.  No account necessary.").appendTo(sliderDiv);
-			getSlide($("<img src='img/keys.png'>"), "Split and passphrase protect private keys for maximum security.").appendTo(sliderDiv);
+			getSlide($("<img src='img/keys.png'>"), "Passphrase-protect and split private keys for maximum security.").appendTo(sliderDiv);
 			getSlide($("<img src='img/checklist.png'>"), "Generate keys securely with automatic environment checks.").appendTo(sliderDiv);
 			
 			function getSlide(img, text) {
@@ -423,18 +423,18 @@ function HomeController(div) {
 			pageDiv.append("<div class='home_description'>Save keys to a file which can be stored on a flash drive, or print to paper to easily create paper wallets.</div>")
 			pageDiv.append($("<img width=750px src='img/print_example.png'>"));
 			
-			// check environment section
+			// split and passphrase section
 			pageDiv.append("<div style='height: 10px'>");
+			pageDiv.append("<div class='home_label'>Passphrase-protect and split private keys for maximum security</div>");
+			pageDiv.append("<div class='home_description'>Encrypt private keys with a passphrase or split them into pieces so funds are not accessible at any one location.  Set how many pieces are needed to recover the keys.  Store one in your safe, one in a bank vault, or one with a trusted family member.</div>")
+			pageDiv.append($("<img style='width:785px; margin-bottom:15px;' src='img/passphrase_input.png'>"));
+			pageDiv.append($("<img style='width:600px;' src='img/split_input.png'>"));
+			
+			// check environment section
+			pageDiv.append("<div style='height: 70px'>");
 			pageDiv.append("<div class='home_label'>Generate keys securely with automatic environment checks</div>");
 			pageDiv.append("<div class='home_description'>Following a few simple recommendations can improve the security of your cryptocurrency.  Automatic environment checks encourage keys to be generated in a secure environment.</div>")
 			pageDiv.append($("<img width=785px src='img/notice_bars.png'>"));
-			
-			// split and passphrase section
-			pageDiv.append("<div style='height: 70px'>");
-			pageDiv.append("<div class='home_label'>Split and passphrase protect private keys for maximum security</div>");
-			pageDiv.append("<div class='home_description'>Split storage into multiple pieces which can be stored independently.  Set how many pieces are needed to recover the keys.  Store one in your safe, one in a bank vault, or one with a trusted family member.</div>")
-			pageDiv.append($("<img style='width:785px; margin-bottom:15px;' src='img/passphrase_input.png'>"));
-			pageDiv.append($("<img style='width:600px;' src='img/split_input.png'>"));
 			
 			// cryptography section
 			pageDiv.append("<div style='height:70px'>");
