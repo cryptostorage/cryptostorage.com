5f5203cc1acbbbee45923825ca164a4054b47e22
woodser <woodser@protonmail.com>
2018-02-15 09:47:32 -0500

    re-order plugins

--- js/AppUtils.js
+++ js/AppUtils.js
@@ -266,16 +266,17 @@ var AppUtils = {
 			AppUtils.plugins.push(new MoneroPlugin());
 			AppUtils.plugins.push(new BitcoinPlugin());
 			AppUtils.plugins.push(new LitecoinPlugin());
+			AppUtils.plugins.push(new NeoPlugin());
 			AppUtils.plugins.push(new DashPlugin());
 			AppUtils.plugins.push(new ZcashPlugin());
 			AppUtils.plugins.push(new EthereumClassicPlugin());
-			AppUtils.plugins.push(new OmiseGoPlugin());
 			AppUtils.plugins.push(new StellarPlugin());
 			AppUtils.plugins.push(new RipplePlugin());
+			AppUtils.plugins.push(new OmiseGoPlugin());
 			AppUtils.plugins.push(new BasicAttentionTokenPlugin());
-			AppUtils.plugins.push(new UbiqPlugin());
 			AppUtils.plugins.push(new WavesPlugin());
 			AppUtils.plugins.push(new BIP39Plugin());
+			AppUtils.plugins.push(new UbiqPlugin());
 		}
 		return AppUtils.plugins;
 	},
