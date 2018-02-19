5ccd73b22b3951655a9ceae6333ccb29879ba894
woodser <woodser@protonmail.com>
2018-02-19 10:51:22 -0500

    bump version to v0.1.5

--- js/AppUtils.js
+++ js/AppUtils.js
@@ -40,7 +40,7 @@ var OperatingSystems = {
 var AppUtils = {
 		
 	// app constants
-	VERSION: "0.1.4",
+	VERSION: "0.1.5",
 	VERSION_POSTFIX: " beta",
 	RUN_MIN_TESTS: true,
 	RUN_FULL_TESTS: false,
