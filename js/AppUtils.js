edcdd4a52d0d17fd6361d24c0fde0d763eff8963
woodser <woodser@protonmail.com>
2018-03-17 09:54:54 -0400

    only duplicate pieces are prevented from being added to imported pieces

--- js/AppUtils.js
+++ js/AppUtils.js
@@ -43,7 +43,7 @@ var AppUtils = {
 	VERSION: "0.2.1",
 	VERSION_POSTFIX: " beta",
 	RUN_MIN_TESTS: true,
-	RUN_FULL_TESTS: true,
+	RUN_FULL_TESTS: false,
 	DEV_MODE: true,
 	DEV_MODE_PASSPHRASE: "abctesting123",
 	DELETE_WINDOW_CRYPTO: false,
