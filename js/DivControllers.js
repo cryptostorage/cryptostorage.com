65bde847bb283fdd4fb3a986dfb44c62975cc6fa
woodser <woodser@protonmail.com>
2018-05-08 21:07:34 -0400

    fix shrinking export div

--- js/DivControllers.js
+++ js/DivControllers.js
@@ -4298,7 +4298,7 @@ function EditorPrintController(div, pieces) {
 		
 		// div setup
 		div.empty();
-		div.addClass("editor_export_div flex_vertical flex_align_items_center")
+		div.addClass("editor_export_div flex_vertical flex_align_center")
 		
 		// header
 		var header = $("<div class='editor_export_header'>").appendTo(div);
